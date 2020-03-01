a2981cbdf3b7c6be3506839a5bcfd850234ff47b
Waxoussou <maxime.abehserra@gmail.com>
2020-03-01 17:56:45 +0100

    add more error control

--- bin/app.js
+++ bin/app.js
@@ -4,7 +4,7 @@ const readline = require('readline');
 const { start_program } = require('../controller')
 const { analyse_args } = require('../lib/utils');
 const { emitter } = require('../lib/EmitterHandler')
-
+const style = require('../lib/colors')
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -18,16 +18,25 @@ if (args.length === 0) {
 else {
     analyse_args(args)
         .then(res => {
-            console.log(res);
-
-            let argEvent = Object.keys(res)[0].split('--')[1];
-            emitter.emit(argEvent, {
-                argument: Object.keys(res),
-                option: res[Object.keys(res)],
-                event: emitter
-            })
+            if (typeof res == 'object') {
+                let argEvent = Object.keys(res)[0].split('--')[1];
+                console.log(argEvent);
+                
+                emitter.emit(argEvent, {
+                    argument: Object.keys(res),
+                    option: res[Object.keys(res)],
+                    event: emitter
+                })
+            } 
+            // else if (res == undefined) {
+            //     throw ('need to indicate an option to use before declaring arguments')
+            // }
         })
         .then(rl.close())
-        .catch(err => console.log(err))
+        .catch(err => process.stdout.write('\n'
+            + style.backgroundColors.red
+            + err
+            + style.reset
+            + '\n\n'))
 }
 
